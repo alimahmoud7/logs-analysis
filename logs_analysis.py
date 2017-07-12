34347484afe9f304e19ce7eddf85d03fb7b38ca7
AliMahmoud7 <ali.mahmoud.engineer@gmail.com>
2017-07-12 18:33:37 +0200

    Improve code quality

--- logs_analysis.py
+++ logs_analysis.py
@@ -4,69 +4,95 @@
 # By `Ali Mahmoud` in `Udacity`
 
 import psycopg2
+import sys
 
 
-def get_query(q, db="news"):
-    """Connect to the database, Execute and return query results."""
-    conn = psycopg2.connect(dbname=db)
-    c = conn.cursor()
-    c.execute(q)
+def connect(db_name="news"):
+    """
+    Connect to the PostgreSQL database.
+    Returns a database connection and cursor.
+    """
+    try:
+        db = psycopg2.connect(dbname=db_name)
+        c = db.cursor()
+        return db, c
+    except psycopg2.Error as e:
+        print("Unable to connect to database")
+        sys.exit(1)
+        # raise e
+
+
+def fetch_query(query):
+    """
+    Connect to the database, query, fetch results,
+    close connection, return results
+    """
+    db, c = connect()
+    c.execute(query)
     results = c.fetchall()
-    conn.close()
+    db.close()
     return results
 
 
-def write_output(file, query, word=''):
-    """Write query results in a text file with some modifications.
+def print_top_articles():
+    """Fetch top articles using helper function, print results"""
+    # 1. What are the most popular three articles of all time?
+    query1 = """
+    SELECT article_title, views
+    FROM   collect
+    LIMIT 3;"""
+    results = fetch_query(query1)
 
-    Args:
-        file: a file object to write in it
-        query: the query that we want to get and print it
-        word: an extra string added to describe numeric types
-    """
-    for i in get_query(query):
-        file.write("- {} -- {} {}\n".format(str(i[0]), str(i[1]), word))
-    file.write('\n\n')
+    print('1. What are the most popular three articles of all time?')
+    for i in results:
+        print("- {} -- {} {}".format(str(i[0]), str(i[1]), 'views'))
 
-# 1. What are the most popular three articles of all time?
-query1 = """
-SELECT article_title, views
-FROM   collect
-LIMIT 3;"""
 
-# 2. Who are the most popular article authors of all time?
-query2 = """
-SELECT author_name, sum(collect.views)::INTEGER AS views
-FROM   collect
-GROUP BY author_name
-ORDER BY views DESC;"""
+def print_top_authors():
+    """ Fetch top authors using helper function, print results"""
+    # 2. Who are the most popular article authors of all time?
+    query2 = """
+    SELECT author_name, sum(collect.views) AS views
+    FROM   collect
+    GROUP BY author_name
+    ORDER BY views DESC;"""
+    results = fetch_query(query2)
 
-# 3. On which days did more than 1% of requests lead to errors?
-query3 = """
-SELECT to_char(day, 'FMMonth DD,YYYY'),  -- For output style only
-       error_percent::NUMERIC
-FROM (SELECT error.day,
-             round((error_count/total_count)*100, 1) AS error_percent
-      FROM (SELECT cast(time AS DATE) AS day,
-                   cast(count(status) AS DECIMAL) AS error_count
-            FROM   log
-            WHERE  status LIKE '404%'
-            GROUP BY day) AS error,
-           (SELECT cast(time AS DATE) AS day,
-                   cast(count(status) AS DECIMAL) AS total_count
-            FROM   log
-            GROUP BY day) AS total
-      WHERE error.day = total.day
-      ORDER BY error_percent DESC) AS percent
-WHERE error_percent > 1.0;"""
+    print('2. Who are the most popular article authors of all time?')
+    for i in results:
+        print("- {} -- {} {}".format(str(i[0]), str(i[1]), 'views'))
+
+
+def print_top_error_days():
+    """ Fetch top error days using helper function, print results"""
+    # 3. On which days did more than 1% of requests lead to errors?
+    query3 = """
+    SELECT to_char(day, 'FMMonth DD,YYYY'),  -- For output style only
+           error_percent
+    FROM (SELECT error.day,
+                 round((error_count/total_count)*100, 1) AS error_percent
+          FROM (SELECT cast(time AS DATE) AS day,
+                       cast(count(status) AS DECIMAL) AS error_count
+                FROM   log
+                WHERE  status LIKE '404%'
+                GROUP BY day) AS error,
+               (SELECT cast(time AS DATE) AS day,
+                       cast(count(status) AS DECIMAL) AS total_count
+                FROM   log
+                GROUP BY day) AS total
+          WHERE error.day = total.day
+          ORDER BY error_percent DESC) AS percent
+    WHERE error_percent > 1.0;"""
+    results = fetch_query(query3)
+
+    print('3. On which days did more than 1% of requests lead to errors?')
+    for i in results:
+        print("- {} -- {} {}".format(str(i[0]), str(i[1]), '% errors'))
+
 
 if __name__ == '__main__':
-    # Writing the output of the queries
-    with open('output.txt', 'w') as f:
-        f.write('1. What are the most popular three articles of all time?\n')
-        write_output(f, query1, 'views')
-        f.write('2. Who are the most popular article authors of all time?\n')
-        write_output(f, query2, 'views')
-        f.write('3. On which days did more than 1% of requests '
-                'lead to errors?\n')
-        write_output(f, query3, '% errors')
+    print_top_articles()
+    print()  # For output style only
+    print_top_authors()
+    print()  # For output style only
+    print_top_error_days()
